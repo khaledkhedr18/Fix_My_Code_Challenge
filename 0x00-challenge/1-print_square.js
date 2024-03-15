5f0b34b2d60297fc286e0d01a4a4fcbcd523e4fc
khaledkhedr18 <khaledkhedr18@gmail.com>
2024-03-15 04:05:02 +0200

    ss

--- 0x00-challenge/1-print_square.js
+++ 0x00-challenge/1-print_square.js
@@ -6,7 +6,8 @@
 */
 
 
-if (process.argv.length <= 2) {
+if (process.argv.length <= 2)
+{
     process.stderr.write("Missing argument\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
@@ -16,7 +17,8 @@ if (process.argv.length <= 2) {
 const size = parseInt(process.argv[2]);
 
 for (let i = 0 ; i < size ; i ++) {
-    for (let j = 0 ; j < size ; j ++) {
+    for (let j = 0 ; j < size ; j ++)
+    {
         process.stdout.write("#");
     }
     process.stdout.write("\n");
